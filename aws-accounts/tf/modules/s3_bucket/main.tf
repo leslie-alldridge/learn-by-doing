18f46b5bdcf96929f2377d99a7363466459ddf72
leslie-alldridge <leslie.alldridge@gmail.com>
2021-11-14 10:11:55 +1300

    Retrict tf state s3 bucket

--- aws-accounts/tf/modules/s3_bucket/main.tf
+++ aws-accounts/tf/modules/s3_bucket/main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "tf_state" {
     enabled = var.versioning
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
